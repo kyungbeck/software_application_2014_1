44424f1f13048ca193358d434128c08b468a0073
Park Jun <pj2417@gmail.com>
2014-05-24 18:02:41 +0000

    message

--- View_card.php
+++ View_card.php
@@ -1,18 +1,16 @@
 <?php
 	//$mysqli=mysqli_connect("localhost", "ser","0000","swproject");
-	$mysqli=mysqli_connect("54.178.195.175", "parkjun", "qqqq", "software_application_2014_1");	
-
+	$mysqli=mysqli_connect("54.178.195.175", "parkjun", "qqqq", "software_application_2014_1");
 	if(mysqli_connect_errno()) { //mysql 연결 실패
 		$message='<p>DB connect error</p>';	
-		
+		exit();
 	}
 	$message="<p><strong>search result</strong></p>";
 	
 	//mysql 연결 성공
 	$search=$_POST["word"];	
 	$check=$_POST["check"];
-	//$message="<p>".$search."</p>";
-
+	$message="<p>".$search."</p>";
 	if($check!=1) {
 		//처음에는 아무런 결과를 띄우지않는다.	
 	}
