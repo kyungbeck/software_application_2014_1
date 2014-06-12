35eb75a2b079be2aec8bcc76592a249872e60b4b
Park Jun <pj2417@gmail.com>
2014-06-12 19:22:12 +0000

    finish

--- View_buscompany.php
+++ View_buscompany.php
@@ -8,7 +8,7 @@
 	}
 	mysqli_query($mysqli, 'set names utf8');	
 //$message="<p><strong>search result</strong></p>";
-	$table="transactional_information";	
+	//$table="transactional_information";	
 	//mysql 연결 성공
 	
 	$frdt=$_POST["frdt"];
@@ -30,8 +30,44 @@
 		$Scompany=stripslashes($cominfo['name']); 
 
 		$message="<p><strong>search result</strong></p>";	
-		$searchsql="SELECT B.company, SUM(changemoney) FROM transactional_information as A, busline_info as B WHERE A.busline = B.busline AND B.company='".$Scompany."'";
-		//특수 검색 시간과 정류장
+		
+		$searchsql2="SELECT company, sum(calculated) FROM company_calcul WHERE busline LIKE '%".$search."%' AND company= '".$Scompany."'";
+        if($frdt && $todt) {
+			if($frdt>$todt) {
+				echo $message="time error";
+				exit();
+			}
+		    $searchsql2.=" AND (offtagtime BETWEEN '".$frdt."' AND '".$todt."')";
+		}
+	    else if($frdt) {
+			$searchsql2.=" AND offtagtime>='".$frdt."'";
+	    }
+	    else if($todt) {
+	        $searhsql2.=" AND offtagtime<='".$todt."'";
+		}
+		$searchsql2.=" GROUP BY company";
+		$searchres2=mysqli_query($mysqli, $searchsql2) or die(mysqli_error($mysqli));
+		$numres2=mysqli_num_rows($searchres2);
+		if($numres2==0) {
+			$message.="<p>There is no search result!</p>";
+		}
+		else {
+		    $message.="<table>";
+		    $message.="<tr><td>*COMPANY</td><td>*총 수입금</td></tr>";
+			while($cardinfo2=mysqli_fetch_array($searchres2)) {
+				$company=stripslashes($cardinfo2["company"]);
+				$money=stripslashes($cardinfo2['sum(calculated)']);
+				$message.="<tr align=right><td>".$company."</td><td>".$money."</td></tr>";
+			}
+			$message.="</table><hr>";
+		}
+
+		
+		
+		
+		
+		$searchsql="SELECT company,busline, calculated, time FROM company_calcul WHERE busline LIKE '%".$search."%' AND company='".$Scompany."'";
+		//특수 검색 시간
 		if($frdt && $todt) {
 			if($frdt2>$todt2) {
 				echo $message="time error"; 
@@ -52,7 +88,7 @@
 			$searchsql.=" AND offtagtime<='".$todt2."'";
 		}
 	
-		$searchsql.=" GROUP BY B.company";
+		//$searchsql.=" GROUP BY B.company";
 		
 		$searchres=mysqli_query($mysqli, $searchsql) or die(mysqli_error($mysqli));
 		$numres=mysqli_num_rows($searchres);
@@ -62,11 +98,13 @@
 		}
 		else {
 			$message.="<table>";
-		$message.="<tr><td>*COMPANY</td><td>*매출</td></tr>";
+		$message.="<tr><td>*COMPANY</td><td>*매출</td><td>*버스노선</td><td>*시간</td></tr>";
 			while($useinfo=mysqli_fetch_array($searchres)) {
 				$company=stripslashes($useinfo['company']);
-				$money=stripslashes($useinfo['SUM(changemoney)']);
-				$message.="<tr align=right><td>".$company."</td><td>".$money."</td></tr>";
+				$money=stripslashes($useinfo['calculated']);
+				$busline=stripslashes($useinfo['busline']);
+				$time=stripslashes($useinfo['time']);
+				$message.="<tr align=right><td>".$company."</td><td>".$money."</td><td>".$busline."</td><td>".$time."</td></tr>";
 			}
 			$message.="</table>";
 			mysqli_free_result($searchres);
